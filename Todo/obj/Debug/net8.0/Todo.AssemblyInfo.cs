93895a22454d280bd38e5bb33a2d86baa908bcf3
Смаль Софія КП-41 <busik865@gmail.com>
2025-03-14 21:43:11 +0200

    File altered.

--- Todo/obj/Debug/net8.0/Todo.AssemblyInfo.cs
+++ Todo/obj/Debug/net8.0/Todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c40ada34c0ead4f0bc3b30617f95d41d2b896e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
