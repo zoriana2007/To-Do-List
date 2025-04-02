05c4fd80ffa45203b66f819cf7d9d338ce10a98c
AnastLavron <anastasiia.lavron@gmail.com>
2025-04-02 18:56:49 +0300

    dis

--- Todo/obj/Debug/net8.0/Todo.AssemblyInfo.cs
+++ Todo/obj/Debug/net8.0/Todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0203e29f7bc3edeb3b6dcfdc8037a9b92d7012a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5a1039f86c7332b1a74a7f4c0880907f136fd32")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
