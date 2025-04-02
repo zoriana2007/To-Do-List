a5a1039f86c7332b1a74a7f4c0880907f136fd32
AnastLavron <anastasiia.lavron@gmail.com>
2025-04-02 17:54:40 +0300

    Revert "Regist/Auth"
    This reverts commit bfc35888bc6611d3c4ca001ab061524dbe99e59b.

--- Todo/obj/Debug/net8.0/Todo.AssemblyInfo.cs
+++ Todo/obj/Debug/net8.0/Todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+027550fec6e8bb8c1afe4cc4601d814a3ea992bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0203e29f7bc3edeb3b6dcfdc8037a9b92d7012a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
